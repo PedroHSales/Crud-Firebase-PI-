b078bd1868d500d030aba2d65a6e7168f4b6e99c
Pedro Henrique Sales <103755714+PedroHSales@users.noreply.github.com>
2024-06-03 19:48:58 -0300

    Delete Crud-Firebase-PI-main directory

--- Crud-Firebase-PI-main/js/view/usuario.js
+++
@@ -1,8 +0,0 @@
-const formUsuario = document.getElementById('formUsuario')
-
-formUsuario.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const email = document.getElementById('email').value
-    const senha = document.getElementById('senha').value
-    novoUsuario(email, senha)
-})
